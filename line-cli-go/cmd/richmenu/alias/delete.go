989d5eec65becce9e024e4c46f757825ab33f628
t-kim <t-kim@planitai.co.jp>
2026-04-21 22:06:11 +0900

    feat(line-cli-go): add richmenu alias delete

---
+++ line-cli-go/cmd/richmenu/alias/delete.go
@@ -0,0 +1,36 @@
+package alias
+
+import (
+	"line-cli-go/internal/client"
+	"line-cli-go/internal/config"
+	"line-cli-go/internal/output"
+
+	"github.com/spf13/cobra"
+)
+
+var deleteCmd = &cobra.Command{
+	Use:   "delete",
+	Short: "Delete a rich menu alias",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		aliasID, _ := cmd.Flags().GetString("alias-id")
+		if aliasID == "" {
+			return &config.ClientError{Msg: "--alias-id is required"}
+		}
+		p := output.NewPrinter(config.JSONMode(), nil)
+		api, err := client.NewMessagingAPI()
+		if err != nil {
+			return err
+		}
+		if _, err := api.DeleteRichMenuAlias(aliasID); err != nil {
+			p.Error(output.ExtractHTTPStatus(err), err.Error())
+			return err
+		}
+		p.Success("Rich menu alias deleted", map[string]string{"richMenuAliasId": aliasID})
+		return nil
+	},
+}
+
+func init() {
+	deleteCmd.Flags().String("alias-id", "", "alias ID (required)")
+	AliasCmd.AddCommand(deleteCmd)
+}
