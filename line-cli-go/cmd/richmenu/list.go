1a6fcf22a44308ce58dfed66e0a2e22913d0f01a
t-kim <t-kim@planitai.co.jp>
2026-04-21 21:16:04 +0900

    feat(line-cli-go): add richmenu list

---
+++ line-cli-go/cmd/richmenu/list.go
@@ -0,0 +1,32 @@
+package richmenu
+
+import (
+	"line-cli-go/internal/client"
+	"line-cli-go/internal/config"
+	"line-cli-go/internal/output"
+
+	"github.com/spf13/cobra"
+)
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List all rich menus",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		p := output.NewPrinter(config.JSONMode(), nil)
+		api, err := client.NewMessagingAPI()
+		if err != nil {
+			return err
+		}
+		resp, err := api.GetRichMenuList()
+		if err != nil {
+			p.Error(output.ExtractHTTPStatus(err), err.Error())
+			return err
+		}
+		p.Raw(resp)
+		return nil
+	},
+}
+
+func init() {
+	RichMenuCmd.AddCommand(listCmd)
+}
