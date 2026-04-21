c8d129763726541cce9e10a954801b05a840c816
t-kim <t-kim@planitai.co.jp>
2026-04-21 21:16:22 +0900

    feat(line-cli-go): add richmenu get-default

---
+++ line-cli-go/cmd/richmenu/get_default.go
@@ -0,0 +1,32 @@
+package richmenu
+
+import (
+	"line-cli-go/internal/client"
+	"line-cli-go/internal/config"
+	"line-cli-go/internal/output"
+
+	"github.com/spf13/cobra"
+)
+
+var getDefaultCmd = &cobra.Command{
+	Use:   "get-default",
+	Short: "Get the default rich menu ID",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		p := output.NewPrinter(config.JSONMode(), nil)
+		api, err := client.NewMessagingAPI()
+		if err != nil {
+			return err
+		}
+		resp, err := api.GetDefaultRichMenuId()
+		if err != nil {
+			p.Error(output.ExtractHTTPStatus(err), err.Error())
+			return err
+		}
+		p.Raw(resp)
+		return nil
+	},
+}
+
+func init() {
+	RichMenuCmd.AddCommand(getDefaultCmd)
+}
